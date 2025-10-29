bcc327a44a946f7924d056bf121c2bb4ad68871f
Kondreddy Balaji Sudarshan Reddy <kondreddy2007@gmail.com>
2025-10-29 09:55:39 +0530

    change: fixed routeanemse

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const {getEvents,getEventById,createEvent,deleteEvent} = require('../controller/Event.controller');
 
-router.get('/events', getEvents);
-router.get('/events/:id', getEventById);
-router.post('/events', createEvent);
-router.delete('/events/:id', deleteEvent);
+router.get('/', getEvents);
+router.get('/:id', getEventById);
+router.post('/', createEvent);
+router.delete('/:id', deleteEvent);
 
 module.exports = router;
